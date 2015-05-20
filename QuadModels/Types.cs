dbaa04134c95c5b8be3bffa31a53df1eba228da7
Scubaboy <robin@robincawsey.plus.com>
2015-05-20 08:43:35 +0100

    setting up myget

---
+++ QuadModels/Types.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadModels
+{
+    public enum CommsOptions
+    {
+        Xbee,
+        GSMModem
+    }
+
+    public enum IMUOpions
+    {
+        DCM,
+        MPU
+    }
+
+    public enum GPSOptions
+    {
+        MKV11
+    }
+
+    public enum AltimeterOptions
+    {
+        GPS,
+        Altic,
+        CGPSALtic
+    }
+}
---
+++ QuadModels/build.bat
@@ -0,0 +1,36 @@
+﻿
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+ 
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+ 
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild QuadModels.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+ 
+mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net40
+ 
+%nuget% pack "QuadModels.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
