d860f01020d8107157dbaf451a75cd9d274db799
Scubaboy <robin@robincawsey.plus.com>
2015-10-30 11:19:00 +0000

    Added quad status class

---
+++ QuadModels/Status.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadModels
+{
+    public class Status
+    {
+        public Status(int quadId, DateTime timeLastHeartbeatReceived, bool heartbeatWithinThreshold)
+        {
+            this.HeartbeatWithinThreshold = heartbeatWithinThreshold;
+            this.TimeLastHeartbeatReceived = timeLastHeartbeatReceived;
+            this.QuadId = quadId;
+
+        }
+
+        public int QuadId { get; private set; }
+        public DateTime TimeLastHeartbeatReceived { get; private set; }
+        public bool HeartbeatWithinThreshold { get; private set; }
+    }
+}
